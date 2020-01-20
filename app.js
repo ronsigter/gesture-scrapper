c9840b21bed75329d3dae84f9c9733cdd0aebfb4
ronsigter <ronchester.mendoza@gmail.com>
2020-01-21 00:59:53 +0800

    Adds port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/asl', async (req, res) => {
     console.log("accessing scrapper...")
