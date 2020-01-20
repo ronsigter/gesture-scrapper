70ce9a2c11e7ad75b1ab78fb750bffc26ba13240
ronsigter <ronchester.mendoza@gmail.com>
2020-01-21 00:55:37 +0800

    Adds console logs

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require('express');
 
 const app = express();
 
+const port = 3000;
+
 app.get('/asl', async (req, res) => {
+    console.log("accessing scrapper...")
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setRequestInterception(true);
@@ -16,7 +19,7 @@ app.get('/asl', async (req, res) => {
           req.continue();
       }
     })
-
+    console.log(`scrapping for ${req.query.search}...`)
     await page.goto('https://courses.startasl.com/mod/glossary/view.php?id=463'); // URL is given by the "user" (your client-side application)
     await page.type('input.form-control', req.query.search)
     await page.click('input.btn.btn-secondary.mr-1')
@@ -34,4 +37,4 @@ app.get('/asl', async (req, res) => {
     await browser.close();
 })
 
-app.listen(4000);
+app.listen(port, () => console.log(`url-scrapper listening on port ${port}!`));
